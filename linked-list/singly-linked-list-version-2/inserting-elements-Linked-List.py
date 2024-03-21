37e744e39dd4d35137c57f44add7cef22f9b5108
sidhu <venkatasidhartha@gmail.com>
2024-03-21 17:41:26 +0530

    linked list function's complete

--- linked-list/singly-linked-list-version-2/inserting-elements-Linked-List.py
+++ linked-list/singly-linked-list-version-2/inserting-elements-Linked-List.py
@@ -95,8 +95,128 @@ class LinkedList:
         Time Complexity: O(n)
         Space Complexity: O(1)
         """ 
-
+    def traverse(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.value)
+            current_node = current_node.next
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """ 
+    def search(self,target):
+        current_node = self.head
+        index = 0
+        while current_node:
+            if current_node.value == target:
+                return index
+            current_node = current_node.next
+            index+=1
+        return -1
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """ 
     
+    def get(self,index):
+        if index == -1:
+            return self.tail 
+        if index < -1 or index > self.length:
+            return None
+        current_node = self.head
+        for i in range(index+1):
+            if i == index:
+                return current_node
+            current_node = current_node.next
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """ 
+    def set_value(self,index,value):
+        # we can try like this 
+        # current_node = self.head
+        # for i in range(index+1):
+        #     if i == index:
+        #         current_node.value = value
+        #         break
+        #     current_node = current_node.next
+
+        # or
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False  
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        popped_node = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            popped_node.next = None
+        self.length-=1
+        return popped_node
+        """
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+        """
+    def pop(self):
+        if self.length == 0:
+            return None
+        popped_node = self.tail
+        if self.length == 1:
+            self.head = self.tail = None
+        else:
+            current_node = self.head
+            while current_node.next is not self.tail:
+                current_node = current_node.next
+            self.tail = current_node
+            current_node.next = None
+        self.length-=1
+        return popped_node
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+
+    def remove(self,index):
+        if self.length == 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length-1:
+            return self.pop()
+        prev_node = self.get(index-1)
+        popped_node = prev_node.next
+        prev_node.next = popped_node.next
+        popped_node.next = None
+        self.length-=1
+        return popped_node
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+    def delete_all(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        """
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+        """
+
+
+
+
+
 
 new_linkedList = LinkedList()
 new_linkedList.append(10)
@@ -107,3 +227,10 @@ new_linkedList.prepend(2)
 print(new_linkedList)
 new_linkedList.insert(1,100) 
 print(new_linkedList)
+# new_linkedList.traverse()
+# print(new_linkedList.search(100))
+# print(new_linkedList.get(0))
+print(new_linkedList.pop_first())
+print(new_linkedList)
+
+
